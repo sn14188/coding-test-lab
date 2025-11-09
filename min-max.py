6034083ba4ed2e149bc1c34b22d2756b63bb84ed
aidenseo <sn14188@gmail.com>
2025-11-09 22:50:34 +0900

    implement min-max solution

---
+++ min-max.py
@@ -0,0 +1,15 @@
+def solution(s: str) -> str:
+    nums_str = s.split(" ")
+
+    nums = []
+    for num_str in nums_str:
+        nums.append(int(num_str))
+        
+    return f"{min(nums)} {max(nums)}"
+
+
+assert solution("1 2 3 4") == "1 4"
+assert solution("-1 -2 -3 -4") == "-4 -1"
+assert solution("-1 -1") == "-1 -1"
+
+print("All tests passed!")
