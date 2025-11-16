8c1fb76804b9f6dbf76267b78624c4a8b9648a32
aidenseo <sn14188@gmail.com>
2025-11-16 17:37:09 +0900

    implement stock-prices solution

--- stock-prices.py
+++ stock-prices.py
@@ -1,19 +1,30 @@
 def solution(prices: list[int]) -> list[int]:
-    answer = []
+    answer = [0] * len(prices)
+    stack = []
+    stack.append(0)
 
-    for i in range(len(prices)):
-        target = prices[i]
+    for idx, price in enumerate(prices):
+        if idx == 0:
+            continue
 
-        duration = 0
-        for j in range(i + 1, len(prices)):
-            if target <= prices[j]:
-                duration += 1
+        top = prices[stack[-1]]
+        if top <= price:
+            stack.append(idx)
+        else:
+            while stack and prices[stack[-1]] > price:
+                top_idx = stack.pop()
+                duration = idx - top_idx
+                answer[top_idx] = duration
+            stack.append(idx)
 
-        answer.append(duration)
+    while stack:
+        idx = stack.pop()
+        answer[idx] = len(prices) - 1 - idx
 
     return answer
 
 
 assert solution([1, 2, 3, 2, 3]) == [4, 3, 1, 1, 0]
+assert solution([1, 2, 7, 10, 12, 4, 2]) == [6, 5, 3, 2, 1, 1, 0]
 
 print("All tests passed!")
