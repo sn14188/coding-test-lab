a6532fd73edcf9ac36716a599b89009f11377835
aidenseo <sn14188@gmail.com>
2025-11-18 09:50:09 +0900

    refactor stock-prices solution

--- stock-prices.py
+++ stock-prices.py
@@ -1,25 +1,26 @@
 def solution(prices: list[int]) -> list[int]:
     answer = [0] * len(prices)
     stack = []
-    stack.append(0)
 
     for idx, price in enumerate(prices):
         if idx == 0:
+            stack.append(0)
             continue
 
         top = prices[stack[-1]]
         if top <= price:
             stack.append(idx)
         else:
-            while stack and prices[stack[-1]] > price:
+            while prices[stack[-1]] > price:
                 top_idx = stack.pop()
                 duration = idx - top_idx
                 answer[top_idx] = duration
             stack.append(idx)
 
+    last_idx = len(prices) - 1
     while stack:
         idx = stack.pop()
-        answer[idx] = len(prices) - 1 - idx
+        answer[idx] = last_idx - idx
 
     return answer
 
