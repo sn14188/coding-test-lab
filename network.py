4e2c6e12de244b209fb2ffb5a1d044eaa9d0d068
aidenseo <sn14188@gmail.com>
2025-12-16 01:36:57 +0900

    implement network solution

--- network.py
+++ network.py
@@ -2,9 +2,30 @@
 
 
 def solution(n: int, computers: list[list[int]]) -> int:
-    assert n == len(computers)
+    visited = [False] * n
+
+    def bfs(start_node):
+        queue = []
+        queue.append(start_node)
+        visited[start_node] = True
+
+        while queue:
+            curr = queue.pop(0)
+
+            for other_computer in range(n):
+                if (
+                    computers[curr][other_computer] == 1
+                    and visited[other_computer] == False
+                ):
+                    queue.append(other_computer)
+                    visited[other_computer] = True
 
     answer = 0
+    for computer in range(n):
+        if visited[computer] == False:
+            answer += 1
+            bfs(computer)
+
     return answer
 
 
