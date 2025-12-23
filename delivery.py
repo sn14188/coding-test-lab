832b98f476853af8fa7a563f9a696a7d5ea9ee1b
aidenseo <sn14188@gmail.com>
2025-12-23 21:35:23 +0900

    implement delivery solution

--- delivery.py
+++ delivery.py
@@ -1,8 +1,49 @@
 # problem url: https://school.programmers.co.kr/learn/courses/30/lessons/12978
 
 
-def solution(N, road, K):
+def solution(N: int, road: list[list[int]], K: int) -> int:
+    graph = dict()
+    for r in road:
+        dep, arr, time = r[0], r[1], r[2]
+
+        if dep not in graph:
+            graph[dep] = [(arr, time)]
+        else:
+            graph[dep].append((arr, time))
+
+        if arr not in graph:
+            graph[arr] = [(dep, time)]
+        else:
+            graph[arr].append((dep, time))
+
+    visited = [False] * (N + 1)
+    dist = [float("inf")] * (N + 1)
+    dist[1] = 0
+
+    for _ in range(N):
+        curr = -1
+        min_dist = float("inf")
+
+        for i in range(1, N + 1):
+            if not visited[i] and dist[i] < min_dist:
+                curr = i
+                min_dist = dist[i]
+
+        if curr == -1:
+            break
+
+        visited[curr] = True
+
+        if curr in graph:
+            for arr, time in graph[curr]:
+                if dist[arr] > dist[curr] + time:
+                    dist[arr] = dist[curr] + time
+
     answer = 0
+    for d in dist:
+        if d <= K:
+            answer += 1
+
     return answer
 
 
