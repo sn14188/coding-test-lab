07804848bbda6a0ee93b4778c74aeccfd64d926f
aidenseo <sn14188@gmail.com>
2025-12-25 23:54:43 +0900

    add vowel-dictionary problem

---
+++ vowel-dictionary.py
@@ -0,0 +1,14 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+
+def solution(word: str):
+    answer = 0
+    return answer
+
+
+assert solution("AAAAE") == 6
+assert solution("AAAE") == 10
+assert solution("I") == 1563
+assert solution("EIO") == 1189
+
+print("All tests passed!")
