123c7f44583540f3f2e038b86a18458f196c433a
aidenseo <sn14188@gmail.com>
2025-12-26 00:52:07 +0900

    implement vowel-dictionary solution

--- vowel-dictionary.py
+++ vowel-dictionary.py
@@ -1,8 +1,26 @@
 # problem url: https://school.programmers.co.kr/learn/courses/30/lessons/84512
 
 
-def solution(word: str):
-    answer = 0
+def solution(word: str) -> int:
+    vowels = ["A", "E", "I", "O", "U"]
+
+    def dfs(curr, counter):
+        if curr:
+            counter += 1
+            if curr == word:
+                return counter, True
+
+        if len(curr) == 5:
+            return counter, False
+
+        for vowel in vowels:
+            counter, found = dfs(curr + vowel, counter)
+            if found:
+                return counter, True
+
+        return counter, False
+
+    answer, _ = dfs("", 0)
     return answer
 
 
