acaee3a8784a3db6cdf3265afb49e48282e55cc9
aidenseo <sn14188@gmail.com>
2025-12-28 13:29:45 +0900

    add hanoi problem

---
+++ tower-of-hanoi.py
@@ -0,0 +1,11 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/12946
+
+
+def solution(n: int) -> list[list[int]]:
+    answer = [[]]
+    return answer
+
+
+assert solution(2) == [[1, 2], [1, 3], [2, 3]]
+
+print("All tests passed!")
