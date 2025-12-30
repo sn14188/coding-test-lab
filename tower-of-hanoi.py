4e8b2d7cce36f8959d8f4124818bad0e9231df92
aidenseo <sn14188@gmail.com>
2025-12-30 09:24:34 +0900

    implement hanoi solution

--- tower-of-hanoi.py
+++ tower-of-hanoi.py
@@ -2,10 +2,24 @@
 
 
 def solution(n: int) -> list[list[int]]:
-    answer = [[]]
+    answer = []
+
+    def dfs(n, start, mid, end):
+        if n == 1:
+            answer.append([start, end])
+            return
+
+        dfs(n - 1, start, end, mid)
+        answer.append([start, end])
+        dfs(n - 1, mid, start, end)
+
+    dfs(n, 1, 2, 3)
+
     return answer
 
 
 assert solution(2) == [[1, 2], [1, 3], [2, 3]]
+assert solution(1) == [[1, 3]]
+assert solution(3) == [[1, 3], [1, 2], [3, 2], [1, 3], [2, 1], [2, 3], [1, 3]]
 
 print("All tests passed!")
