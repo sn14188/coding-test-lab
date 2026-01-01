18e0ed898314c9f777f97d0665dd13ba32071ae4
aidenseo <sn14188@gmail.com>
2026-01-02 04:30:57 +0900

    add test cases

--- pre-to-post.py
+++ pre-to-post.py
@@ -20,6 +20,23 @@ def solution(nums: list[int]) -> list[int]:
     return solution(left) + solution(right) + [root]
 
 
+assert solution([]) == []
+assert solution([50]) == [50]
+assert solution([50, 60]) == [60, 50]
+assert solution([50, 30, 24, 5]) == [
+    5,
+    24,
+    30,
+    50,
+]
+assert solution([50, 30, 24, 5, 28, 45]) == [
+    5,
+    28,
+    24,
+    45,
+    30,
+    50,
+]
 assert solution([50, 30, 24, 5, 28, 45, 98, 52, 60]) == [
     5,
     28,
