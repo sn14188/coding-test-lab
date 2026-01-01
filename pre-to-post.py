7beddb4467c52312684a4b73d7412dd86175be31
aidenseo <sn14188@gmail.com>
2026-01-02 04:07:54 +0900

    add pre-to-post problem

---
+++ pre-to-post.py
@@ -0,0 +1,21 @@
+# problem url: https://www.acmicpc.net/problem/5639
+
+
+def solution(nums: list[int]) -> list[int]:
+    answer = []
+    return answer
+
+
+assert solution([50, 30, 24, 5, 28, 45, 98, 52, 60]) == [
+    5,
+    28,
+    24,
+    45,
+    30,
+    60,
+    52,
+    98,
+    50,
+]
+
+print("All tests passed!")
