a7a6091d0992cb378b1fc92770b6b122d59d09ec
aidenseo <sn14188@gmail.com>
2026-01-06 22:39:23 +0900

    better logic

--- 2026/0106-carpet.py
+++ 2026/0106-carpet.py
@@ -3,24 +3,18 @@
 
 def solution(brown: int, yellow: int) -> list[int]:
     total_tiles = brown + yellow
-    candidates = []
-
-    yellow_height = 1
-    while yellow >= yellow_height:
-        if yellow % yellow_height == 0:
-            yellow_width = yellow // yellow_height
-            candidates.append([yellow_width, yellow_height])
-
-        yellow_height += 1
 
     answer = []
-    for yellow_width, yellow_height in candidates:
-        carpet_width = yellow_width + 2
-        carpet_height = yellow_height + 2
+    for yellow_width in range(yellow, 0, -1):
+        if yellow % yellow_width == 0:
+            yellow_height = yellow // yellow_width
 
-        if carpet_width * carpet_height == total_tiles:
-            answer += [carpet_width, carpet_height]
-            break
+            carpet_width = yellow_width + 2
+            carpet_height = yellow_height + 2
+
+            if carpet_width * carpet_height == total_tiles:
+                answer += [carpet_width, carpet_height]
+                break
 
     return answer
 
