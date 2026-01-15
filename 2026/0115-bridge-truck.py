368986f0ad00bf6890792da807fee279fd10a70e
aidenseo <sn14188@gmail.com>
2026-01-15 11:07:14 +0900

    add bridge-truck problem

---
+++ 2026/0115-bridge-truck.py
@@ -0,0 +1,13 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/42583
+
+
+def solution(bridge_length: int, weight: int, truck_weights: list[int]) -> int:
+    answer = 0
+    return answer
+
+
+assert solution(2, 10, [7, 4, 5, 6]) == 8
+assert solution(100, 100, [10]) == 101
+assert solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]) == 110
+
+print("All tests passed!")
