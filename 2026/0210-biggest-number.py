bd80f40185cae43f9e3ccbadc5d70a0647af8ebc
aidenseo <sn14188@gmail.com>
2026-02-10 09:42:06 +0900

    add biggest-number problem

---
+++ 2026/0210-biggest-number.py
@@ -0,0 +1,38 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/42746
+
+from itertools import permutations
+
+
+# selection sort
+def solution_ss(numbers: list[int]) -> str:
+    str_numbers = list(map(str, numbers))
+
+    for i in range(len(str_numbers)):
+        best = i
+        for j in range(i + 1, len(str_numbers)):
+            if str_numbers[best] + str_numbers[j] < str_numbers[j] + str_numbers[best]:
+                best = j
+
+        str_numbers[i], str_numbers[best] = str_numbers[best], str_numbers[i]
+
+    answer = "".join(str_numbers)
+
+    return answer
+
+
+# brute force
+def solution_bf(numbers: list[int]) -> str:
+    biggest_num = 0
+
+    for permutation in permutations(numbers):
+        num = int("".join(map(str, permutation)))
+        biggest_num = max(num, biggest_num)
+
+    answer = str(biggest_num)
+
+    return answer
+
+
+assert solution([6, 10, 2]) == "6210"
+assert solution([3, 30, 34, 5, 9]) == "9534330"
+print("All tests passed!")
