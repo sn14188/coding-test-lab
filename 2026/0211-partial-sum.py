3066c92fc12e8ee45413000f5006d841a3de5385
aidenseo <sn14188@gmail.com>
2026-02-12 00:09:14 +0900

    refactor partial-sum solution

--- 2026/0211-partial-sum.py
+++ 2026/0211-partial-sum.py
@@ -1,7 +1,32 @@
 # problem url: https://www.acmicpc.net/problem/1806
 
+import sys
+
+input = sys.stdin.readline
+
 
 def solution(numbers: list[int], target: int) -> int:
+    min_length = float("inf")
+
+    start_idx = 0
+    partial_sum = 0
+
+    for end_idx in range(len(numbers)):
+        partial_sum += numbers[end_idx]
+
+        if partial_sum >= target:
+            while partial_sum >= target and start_idx <= end_idx:
+                min_length = min(min_length, end_idx - start_idx + 1)
+                partial_sum -= numbers[start_idx]
+                start_idx += 1
+
+    if min_length == float("inf"):
+        return 0
+
+    return min_length
+
+
+def solution_timeout(numbers: list[int], target: int) -> int:
     start_idx, end_idx = 0, 0
     min_length = float("inf")
 
@@ -27,3 +52,13 @@ assert solution([5, 1, 3, 5, 10, 7, 4, 9, 2, 8], 15) == 2
 assert solution([5, 1, 3, 5, 10, 7, 4, 9, 2, 8], 54) == 10
 
 print("All tests passed!")
+
+
+# def main() -> None:
+#     _, target = map(int, input().split())
+#     numbers = list(map(int, input().split()))
+#     print(solution(numbers, target))
+
+
+# if __name__ == "__main__":
+#     main()
