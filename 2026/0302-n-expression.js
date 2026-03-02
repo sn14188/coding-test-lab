e42a73998f60a970df4e9239c537859c619518e1
aidenseo <sn14188@gmail.com>
2026-03-02 18:33:09 +0900

    add n-expression problems

---
+++ 2026/0302-n-expression.js
@@ -0,0 +1,9 @@
+// problem url: https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+function solution(N, number) {
+  var answer = 0;
+  return answer;
+}
+
+console.assert(solution(5, 12) === 4, "case 1");
+console.assert(solution(2, 11) === 3, "case 2");
---
+++ 2026/0302-n-expression.py
@@ -0,0 +1,12 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+
+def solution(N: int, number: int) -> int:
+    answer = 0
+    return answer
+
+
+assert solution(5, 12) == 4
+assert solution(2, 11) == 3
+
+print("All tests passed!")
