c68aa76a8238bf5ee97c5f2478d3d29524a5ad05
aidenseo <sn14188@gmail.com>
2026-03-02 21:41:06 +0900

    implement n-expression solution

--- 2026/0302-n-expression.py
+++ 2026/0302-n-expression.py
@@ -2,8 +2,36 @@
 
 
 def solution(N: int, number: int) -> int:
-    answer = 0
-    return answer
+    def combine(set_1: set[int], set_2: set[int]) -> set[int]:
+        result = set()
+
+        for a in set_1:
+            for b in set_2:
+                result.add(a + b)
+                result.add(a - b)
+                result.add(a * b)
+
+                if b != 0:
+                    result.add(a // b)
+
+        return result
+
+    if N == number:
+        return 1
+
+    dp = [set() for _ in range(9)]
+    dp[1].add(N)
+
+    for i in range(2, 9):
+        dp[i].add(int(str(N) * i))
+
+        for j in range(1, i):
+            dp[i] = dp[i] | combine(dp[j], dp[i - j])
+
+            if number in dp[i]:
+                return i
+
+    return -1
 
 
 assert solution(5, 12) == 4
