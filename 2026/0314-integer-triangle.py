0f5ffc478879631cb5f10b94354b749aec913b1b
aidenseo <sn14188@gmail.com>
2026-03-14 11:44:02 +0900

    add integer-triangle problem

---
+++ 2026/0314-integer-triangle.py
@@ -0,0 +1,19 @@
+# problem url: https://www.acmicpc.net/problem/1932
+
+
+def max_triangle_path(triangle: list[list[int]]) -> int:
+    return -1
+
+
+assert (
+    max_triangle_path(
+        [7],
+        [3, 8],
+        [8, 1, 0],
+        [2, 7, 4, 4],
+        [4, 5, 2, 6, 5],
+    )
+    == 30
+)
+
+print("All tests passed!")
