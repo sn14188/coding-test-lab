856dacec9c483434ff44b8c44f23b1fb8be38532
aidenseo <sn14188@gmail.com>
2026-03-22 11:13:28 +0900

    implement lifeboat solution

---
+++ 2026/0321-lifeboat.py
@@ -0,0 +1,30 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/42885
+
+
+def solution(people: list[int], limit: int) -> int:
+    people.sort()
+
+    left = 0
+    right = len(people) - 1
+
+    answer = 0
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            answer += 1
+            left += 1
+            right -= 1
+        else:
+            answer += 1
+            right -= 1
+
+    return answer
+
+
+assert solution([70, 50, 80, 50], 100) == 3
+assert solution([70, 80, 50], 100) == 3
+
+assert solution([40, 50], 100) == 1
+assert solution([50, 50], 100) == 1
+assert solution([50, 51], 100) == 2
+
+print("All tests passed!")
