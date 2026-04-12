d36bd9ce5a5f596e3d9d24755b8fd9f259a8e375
aidenseo <sn14188@gmail.com>
2026-04-12 13:59:51 +0900

    implement security-manager solution

--- 2026/0412-security-manager.py
+++ 2026/0412-security-manager.py
@@ -2,11 +2,46 @@
 
 
 def solution(sequence: str) -> bool:
-    return False
+    if len(sequence) % 2 == 1:
+        return False
+
+    max_in, min_in = 0, 0
+
+    for s in sequence:
+        if s == "(":
+            max_in += 1
+            min_in += 1
+        elif s == ")":
+            max_in -= 1
+            min_in -= 1
+        else:
+            max_in += 1
+            min_in -= 1
+
+        if max_in < 0:
+            return False
+
+        if min_in < 0:
+            min_in = 0
+
+        # print("")
+        # print(f"max_in: {max_in}")
+        # print(f"min_in: {min_in}")
+
+    return min_in == 0
 
 
 assert solution("(?(??)") == True
 assert solution("(((())") == False
 assert solution("?????") == False
+
+# added cases
+assert solution("()") == True
+assert solution("((") == False
+assert solution("))") == False
+assert solution("(()())") == True
+
+assert solution("??") == True
+assert solution("???") == False
 
 print("All tests passed!")
