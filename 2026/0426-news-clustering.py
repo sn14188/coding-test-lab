f9514ce1254a83685d2cb05f02f5347f35da8807
aidenseo <sn14188@gmail.com>
2026-04-26 16:53:13 +0900

    add news-clustering problem

---
+++ 2026/0426-news-clustering.py
@@ -0,0 +1,14 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/17677
+
+
+def solution(str1: str, str2: str) -> int:
+    answer = 0
+    return answer
+
+
+assert solution("FRANCE", "french") == 16384
+assert solution("handshake", "shake hands") == 65536
+assert solution("aa1+aa2", "AAAA12") == 43690
+assert solution("E=M*C^2", "e=m*c^2") == 65536
+
+print("All tests passed!")
