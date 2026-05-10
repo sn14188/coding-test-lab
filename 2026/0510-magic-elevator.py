7485793b743aae31004e960a1e36c0bcc39090a2
aidenseo <sn14188@gmail.com>
2026-05-10 16:12:06 +0900

    add magic-elevator problem

---
+++ 2026/0510-magic-elevator.py
@@ -0,0 +1,12 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/148653
+
+
+def solution(storey: int) -> int:
+    answer = 0
+    return answer
+
+
+assert solution(16) == 6
+assert solution(2554) == 16
+
+print("All tests passed!")
