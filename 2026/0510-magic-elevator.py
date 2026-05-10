2e66080fbac451af3e504f2b50e50e5554356132
aidenseo <sn14188@gmail.com>
2026-05-10 17:25:35 +0900

    implement magic-elevator solution

--- 2026/0510-magic-elevator.py
+++ 2026/0510-magic-elevator.py
@@ -3,10 +3,29 @@
 
 def solution(storey: int) -> int:
     answer = 0
+
+    while storey > 0:
+        current = storey % 10
+
+        if current < 5:
+            answer += current
+        elif current > 5:
+            answer += 10 - current
+            storey += 10 - current
+        else:
+            upper = (storey // 10) % 10
+            answer += 5
+            if upper >= 5:
+                storey += 5
+
+        storey //= 10
+
     return answer
 
 
 assert solution(16) == 6
 assert solution(2554) == 16
+assert solution(15) == 6
+assert solution(95) == 6
 
 print("All tests passed!")
