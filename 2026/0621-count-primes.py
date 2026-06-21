40a7545966efe8ad1f7b7df1b4bf881899a98c62
aidenseo <sn14188@gmail.com>
2026-06-21 15:01:07 +0900

    add count-primes problem

---
+++ 2026/0621-count-primes.py
@@ -0,0 +1,12 @@
+# problem url: https://school.programmers.co.kr/learn/courses/30/lessons/92335
+
+
+def solution(n: int, k: int) -> int:
+    answer = -1
+    return answer
+
+
+assert solution(437674, 3) == 3
+assert solution(110011, 10) == 2
+
+print("All tests passed!")
