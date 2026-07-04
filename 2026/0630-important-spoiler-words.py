7cf7cd478d0af36859e48f57fb77b0cfddfceb04
aidenseo <sn14188@gmail.com>
2026-07-05 04:51:46 +0900

    implement important-spoiler-words solution

--- 2026/0630-important-spoiler-words.py
+++ 2026/0630-important-spoiler-words.py
@@ -1,8 +1,40 @@
 # problem url: https://school.programmers.co.kr/learn/courses/30/lessons/468370
 
 
+def overlaps_any_spoiler(word_start, word_end, spoiler_ranges):
+    for spoiler_start, spoiler_end in spoiler_ranges:
+        if word_start <= spoiler_end and spoiler_start <= word_end:
+            return True
+
+    return False
+
+
 def solution(message: str, spoiler_ranges: list[list[int]]) -> int:
     answer = 0
+
+    words = []
+    tokens = message.split(" ")
+
+    current_index = 0
+    for token in tokens:
+        word_start = current_index
+        word_end = current_index + len(token) - 1
+        words.append((token, word_start, word_end))
+        current_index = word_end + 2
+
+    clean_word_values = set()
+    for word_value, word_start, word_end in words:
+        if not overlaps_any_spoiler(word_start, word_end, spoiler_ranges):
+            clean_word_values.add(word_value)
+
+    counted_word_values = set()
+    for word_value, word_start, word_end in words:
+        if word_value in clean_word_values or word_value in counted_word_values:
+            continue
+        if overlaps_any_spoiler(word_start, word_end, spoiler_ranges):
+            answer += 1
+            counted_word_values.add(word_value)
+
     return answer
 
 
